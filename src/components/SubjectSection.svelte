80cc4876d5e2bac658333d865ef80ffc92407730
123654-dev <72887184+123654-dev@users.noreply.github.com>
2025-03-03 13:52:21 +0100

    initial commit

---
+++ src/components/SubjectSection.svelte
@@ -0,0 +1,25 @@
+<script>
+  let { children, title } = $props();
+</script>
+
+<div id="subject-wrapper">
+  <div id="subject-style-wrapper">
+    <h2>{title}</h2>
+    {@render children()}
+  </div>
+</div>
+
+<style>
+  #subject-style-wrapper {
+    padding: 25px;
+    border: 1px solid black;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #subject-wrapper {
+    width: 25%;
+    padding: 5px;
+  } 
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import SubjectSection from "../components/SubjectSection.svelte";
+</script>
+
+<h1>Assignment Viewer 2.0</h1>
+
+<div id="container">
+  <SubjectSection title="Analysis I">
+    <h1>Servus</h1>
+  </SubjectSection>
+  <SubjectSection title="Algorithmen und Wahrscheinlichkeit">
+    <h1>Servus</h1>
+  </SubjectSection>
+  <SubjectSection title="Digital Design and Computer Architecture">
+    <h1>Servus</h1>
+  </SubjectSection>
+  <SubjectSection title="Parallele Programmierung">
+    <h1>Servus</h1>
+  </SubjectSection>
+</div>
+
+<style>
+    #container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+</style>
