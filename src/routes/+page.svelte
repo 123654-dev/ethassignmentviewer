97b628853d8091efdcbdc914ded0105670401487
123654-dev <72887184+123654-dev@users.noreply.github.com>
2025-03-26 14:40:54 +0100

    Added disclaimer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,13 @@
   <SubjectSection title="PProg" subject={Subject.PPROG}></SubjectSection>
 </div>
 
-<div style="height: 500px;"></div>
+<div style="width: 450px; color: gray; margin: auto; margin-top: 25px;">
+  This website is not affiliated with ETH Zürich. It only scrapes some data to
+  display all public assignment and website URLs in one place. Please refer to
+  the official websites for the most up-to-date information!
+</div>
+
+<div style="height: 100px;"></div>
 
 <style>
   @import "../app.css";
